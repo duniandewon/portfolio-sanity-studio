cef6b267b83d70f227379b10c7571353f151e03d
duniandewon <duniandewon@gmail.com>
2025-08-14 17:48:30 +0700

    Create porjects schema

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,1 +1,3 @@
-export const schemaTypes = []
+import {projectType} from './projectTypes'
+
+export const schemaTypes = [projectType]
---
+++ schemaTypes/projectTypes.ts
@@ -0,0 +1,36 @@
+import {defineField, defineType} from 'sanity'
+
+export const projectType = defineType({
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {source: 'title'},
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'publishedAt',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'tech',
+      type: 'array',
+      of: [{type: 'string'}],
+    }),
+    defineField({
+      name: 'body',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+  ],
+})
